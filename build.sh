fadac1173594277488590cdb1c47f860a07325e4
Gaurav Chaudhary <jaatsaab735@gmail.com>
2025-05-16 01:29:46 +0530

    fixing dependencie6

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
+python manage.py makemigrations basic_app
+python manage.py migrate basic_app
 python manage.py migrate
 python manage.py shell -c 'from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username="admin").exists() or User.objects.create_superuser("admin", "admin@example.com", "admin123")'
 python manage.py shell -c 'from basic_app.models import DoctorProfile; DoctorProfile.objects.get_or_create(full_name="Dr. John Doe", defaults={"phone_number": "1234567890", "speciality": "General", "qualification": "MD", "location": "vellore", "hospital_name": "Care Hospital", "doctor_username": "doctor1"})'
